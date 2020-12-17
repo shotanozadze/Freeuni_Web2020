d78779312ed1250d77aa25f6fde08ad1fcdd1988
shotanozadze <snoza17@freeuni.edu.ge>
2020-12-17 14:39:22 +0400

    Match Selector

--- JS/darkTheme.js
+++ JS/darkTheme.js
@@ -9,73 +9,73 @@ let bars = document.getElementsByClassName('barsection');
 let barnames = document.getElementsByClassName('barname');
 
 moon.addEventListener('click', function(){
-                                if(!isDark){
-                                    bg.style.background = '#232323';
-                                    maincont.style.background = "black";
-                                    maincont.style.border = "1px solid #2f3336";
-                                    for (let i=0; i<matchTabs.length; i++){
-                                        let rows = matchTabs[i].getElementsByTagName('tr');
-                                        for(let j=0; j<rows.length; j++){
-                                            if (j===0){
-                                                rows[j].style.background = "#2f3336";
-                                                rows[j].style.border = "1px solid #2f3336";
-                                            } else{
-                                                rows[j].style.border = "1px solid #2f3336";
-                                                rows[j].style.color = "white";
-                                                rows[j].getElementsByTagName('td')[0].style.borderRight = "1px solid #2f3336";
-                                            }
-                                        }
-                                    }
-                                    let navRows = nav.getElementsByTagName('a');
-                                    for (let i=0; i<navRows.length; i++){
-                                        navRows[i].style.color = 'white';
-                                        navRows[i].style.borderTop = '1px solid #2f3336';
-                                    }
-                                    for (let i=0; i<bars.length; i++){
-                                        bars[i].style.border = "1px solid #2f3336";
-                                    }
-                                    for (let i=0; i<barnames.length; i++){
-                                        barnames[i].style.background = "#232323";
-                                        barnames[i].style.color = "white";
-                                    }
-                                    footer.style.borderTop = "1px solid #2f3336";
-                                    footer.style.color = "white";
-                                    moon.style.background = "#f6c246";
-                                    document.querySelector('#main > div.right_bg').style.borderLeft = '1px solid #2f3336';
-                                    isDark = true;
-                                }else{
-                                    bg.style.background = "#f7f7f7";
-                                    maincont.style.background = "white";
-                                    maincont.style.border = "1px solid #e2e2e2";
-                                    for (let i=0; i<matchTabs.length; i++){
-                                        let rows = matchTabs[i].getElementsByTagName('tr');
-                                        for(let j=0; j<rows.length; j++){
-                                            if(j===0) {
-                                                rows[j].style.background = "#33464e";
-                                                rows[j].style.border = "1px solid #e2e2e2";
-                                            } else{
-                                                rows[j].style.border = "1px solid #e2e2e2";
-                                                rows[j].style.color = "#2d2d2d";
-                                                rows[j].getElementsByTagName('td')[0].style.borderRight = "1px solid #e2e2e2";
-                                            }
-                                        }
-                                    }
-                                    let navRows = nav.getElementsByTagName('a');
-                                    for (let i=0; i<navRows.length; i++){
-                                        navRows[i].style.color = 'black';
-                                        navRows[i].style.borderTop = '1px solid #e2e2e2';
-                                    }
-                                    for (let i=0; i<bars.length; i++){
-                                        bars[i].style.border = "1px solid #e2e2e2";
-                                    }
-                                    for (let i=0; i<barnames.length; i++){
-                                        barnames[i].style.background = "#33464e";
-                                        barnames[i].style.color = "#e2e2e2";
-                                    }
-                                    footer.style.borderTop = "1px solid #e2e2e2";
-                                    footer.style.color = "#05172099";
-                                    moon.style.background = "white";
-                                    document.querySelector('#main > div.right_bg').style.borderLeft = '1px solid #e7e7e7';
-                                    isDark = false;
-                                }
-                            });
+    if(!isDark){
+        bg.style.background = '#232323';
+        maincont.style.background = "black";
+        maincont.style.border = "1px solid #2f3336";
+        for (let i=0; i<matchTabs.length; i++){
+            let rows = matchTabs[i].getElementsByTagName('tr');
+            for(let j=0; j<rows.length; j++){
+                if (j===0){
+                    rows[j].style.background = "#2f3336";
+                    rows[j].style.border = "1px solid #2f3336";
+                } else{
+                    rows[j].style.border = "1px solid #2f3336";
+                    rows[j].style.color = "white";
+                    rows[j].getElementsByTagName('td')[0].style.borderRight = "1px solid #2f3336";
+                }
+            }
+        }
+        let navRows = nav.getElementsByTagName('a');
+        for (let i=0; i<navRows.length; i++){
+            navRows[i].style.color = 'white';
+            navRows[i].style.borderTop = '1px solid #2f3336';
+        }
+        for (let i=0; i<bars.length; i++){
+            bars[i].style.border = "1px solid #2f3336";
+        }
+        for (let i=0; i<barnames.length; i++){
+            barnames[i].style.background = "#232323";
+            barnames[i].style.color = "white";
+        }
+        footer.style.borderTop = "1px solid #2f3336";
+        footer.style.color = "white";
+        moon.style.background = "#f6c246";
+        document.querySelector('#main > div.right_bg').style.borderLeft = '1px solid #2f3336';
+        isDark = true;
+    }else{
+        bg.style.background = "#f7f7f7";
+        maincont.style.background = "white";
+        maincont.style.border = "1px solid #e2e2e2";
+        for (let i=0; i<matchTabs.length; i++){
+            let rows = matchTabs[i].getElementsByTagName('tr');
+            for(let j=0; j<rows.length; j++){
+                if(j===0) {
+                    rows[j].style.background = "#33464e";
+                    rows[j].style.border = "1px solid #e2e2e2";
+                } else{
+                    rows[j].style.border = "1px solid #e2e2e2";
+                    rows[j].style.color = "#2d2d2d";
+                    rows[j].getElementsByTagName('td')[0].style.borderRight = "1px solid #e2e2e2";
+                }
+            }
+        }
+        let navRows = nav.getElementsByTagName('a');
+        for (let i=0; i<navRows.length; i++){
+            navRows[i].style.color = 'black';
+            navRows[i].style.borderTop = '1px solid #e2e2e2';
+        }
+        for (let i=0; i<bars.length; i++){
+            bars[i].style.border = "1px solid #e2e2e2";
+        }
+        for (let i=0; i<barnames.length; i++){
+            barnames[i].style.background = "#33464e";
+            barnames[i].style.color = "#e2e2e2";
+        }
+        footer.style.borderTop = "1px solid #e2e2e2";
+        footer.style.color = "#05172099";
+        moon.style.background = "white";
+        document.querySelector('#main > div.right_bg').style.borderLeft = '1px solid #e7e7e7';
+        isDark = false;
+    }
+});
---
+++ JS/matchinfo.js
@@ -0,0 +1,43 @@
+let squad = document.querySelector('#left > div.mSelector > div.matchSelectorMain');
+let goals = document.querySelector('#left > div.mSelector > div:nth-child(2)');
+let details = document.querySelector('#left > div.mSelector > div:nth-child(3)');
+
+let squadTable = document.querySelector('#left > table.lineups');
+let goalsTable = document.querySelector('#left > table.matchSummary');
+//let detailsTable = document.querySelector();
+
+squad.addEventListener('click', function(){
+    squadTable.style.display = "table";
+    goalsTable.style.display = "none";
+
+    squad.style.color = "#232323";
+    squad.style.backgroundColor = "#f6c246";
+    goals.style.color = "#dedfdf";
+    goals.style.backgroundColor = "#33464e";
+    details.style.color = "#dedfdf";
+    details.style.backgroundColor = "#33464e";
+});
+
+goals.addEventListener('click', function(){
+    goalsTable.style.display = "table";
+    squadTable.style.display = "none";
+
+    goals.style.color = "#232323";
+    goals.style.backgroundColor = "#f6c246";
+    squad.style.color = "#dedfdf";
+    squad.style.backgroundColor = "#33464e";
+    details.style.color = "#dedfdf";
+    details.style.backgroundColor = "#33464e";
+});
+
+details.addEventListener('click', function(){
+    squadTable.style.display = "none";
+    goalsTable.style.display = "none";
+
+    details.style.color = "#232323";
+    details.style.backgroundColor = "#f6c246";
+    goals.style.color = "#dedfdf";
+    goals.style.backgroundColor = "#33464e";
+    squad.style.color = "#dedfdf";
+    squad.style.backgroundColor = "#33464e";
+});
