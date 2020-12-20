78a1abb5106c9aa3aa341f6b7910b31123046ab3
shotanozadze <snoza17@freeuni.edu.ge>
2020-12-20 13:15:25 +0400

    Mobile Dark Theme

--- JS/darkTheme.js
+++ JS/darkTheme.js
@@ -9,91 +9,198 @@ let nav = document.querySelector('#right > div:nth-child(1) > div.navItem');
 let bars = document.getElementsByClassName('barsection');
 let barnames = document.getElementsByClassName('barname');
 
-function dark(){
-    if(!isDark){
-        bg.style.background = '#232323';
-        maincont.style.background = "black";
-        
-        const mq = window.matchMedia( "(max-width: 1000px)" );
-        if(!mq.matches){
-            maincont.style.border = "1px solid #2f3336";
-        }
+function makeDark(){
+    bg.style.background = '#232323';
+    maincont.style.background = "black";
+    maincont.style.border = "1px solid #2f3336";
 
-        for (let i=0; i<matchTabs.length; i++){
-            let rows = matchTabs[i].getElementsByTagName('tr');
-            for(let j=0; j<rows.length; j++){
-                if (j===0){
-                    rows[j].style.background = "#2f3336";
-                    rows[j].style.border = "1px solid #2f3336";
-                } else{
-                    rows[j].style.border = "1px solid #2f3336";
-                    rows[j].style.color = "white";
-                    rows[j].getElementsByTagName('td')[0].style.borderRight = "1px solid #2f3336";
-                }
+    for (let i=0; i<matchTabs.length; i++){
+        let rows = matchTabs[i].getElementsByTagName('tr');
+        for(let j=0; j<rows.length; j++){
+            if (j===0){
+                rows[j].style.background = "#2f3336";
+                rows[j].style.border = "1px solid #2f3336";
+            } else{
+                rows[j].style.border = "1px solid #2f3336";
+                rows[j].style.color = "white";
+                rows[j].getElementsByTagName('td')[0].style.borderRight = "1px solid #2f3336";
             }
         }
-        let navRows = nav.getElementsByTagName('a');
-        for (let i=0; i<navRows.length; i++){
-            navRows[i].style.color = 'white';
-            navRows[i].style.borderTop = '1px solid #2f3336';
+    }
+    let navRows = nav.getElementsByTagName('a');
+    for (let i=0; i<navRows.length; i++){
+        navRows[i].style.color = 'white';
+        navRows[i].style.borderTop = '1px solid #2f3336';
+    }
+    for (let i=0; i<bars.length; i++){
+        bars[i].style.border = "1px solid #2f3336";
+    }
+    for (let i=0; i<barnames.length; i++){
+        barnames[i].style.background = "#232323";
+        barnames[i].style.color = "white";
+    }
+    footer.style.borderTop = "1px solid #2f3336";
+    footer.style.color = "white";
+    moon.style.background = "#f6c246";
+    moon.style.filter = "none";
+    mobMoon.style.background = "#f6c246";
+    mobMoon.style.filter = "none";
+    document.querySelector('#main > div.right_bg').style.borderLeft = '1px solid #2f3336';
+    isDark = true;
+}
+
+function makeLight(){
+    bg.style.background = "#f7f7f7";
+    maincont.style.background = "white";
+    const mq = window.matchMedia("(max-width: 1000px)");
+    if(!mq.matches){
+        maincont.style.border = "1px solid #e2e2e2";
+    }
+    for (let i=0; i<matchTabs.length; i++){
+        let rows = matchTabs[i].getElementsByTagName('tr');
+        for(let j=0; j<rows.length; j++){
+            if(j===0) {
+                rows[j].style.background = "#33464e";
+                rows[j].style.border = "1px solid #e2e2e2";
+            } else{
+                rows[j].style.border = "1px solid #e2e2e2";
+                rows[j].style.color = "#2d2d2d";
+                rows[j].getElementsByTagName('td')[0].style.borderRight = "1px solid #e2e2e2";
+            }
         }
-        for (let i=0; i<bars.length; i++){
-            bars[i].style.border = "1px solid #2f3336";
+    }
+    let navRows = nav.getElementsByTagName('a');
+    for (let i=0; i<navRows.length; i++){
+        navRows[i].style.color = 'black';
+        navRows[i].style.borderTop = '1px solid #e2e2e2';
+    }
+    for (let i=0; i<bars.length; i++){
+        bars[i].style.border = "1px solid #e2e2e2";
+    }
+    for (let i=0; i<barnames.length; i++){
+        barnames[i].style.background = "#33464e";
+        barnames[i].style.color = "#e2e2e2";
+    }
+    footer.style.borderTop = "1px solid #e2e2e2";
+    footer.style.color = "#05172099";
+    moon.style.background = "white";
+    moon.style.filter = "invert(100%)";
+    mobMoon.style.background = "white";
+    mobMoon.style.filter = "invert(100%)";
+    document.querySelector('#main > div.right_bg').style.borderLeft = '1px solid #e7e7e7';
+    isDark = false;
+}
+
+function makeDarkMob(){
+    bg.style.background = '#232323';
+    maincont.style.background = "black";
+    
+    for (let i=0; i<matchTabs.length; i++){
+        let rows = matchTabs[i].getElementsByTagName('tr');
+        for(let j=0; j<rows.length; j++){
+            if (j===0){
+                rows[j].style.background = "#2f3336";
+                rows[j].style.border = "2px solid #2f3336";
+            } else{
+                rows[j].style.border = "2px solid #2f3336";
+                rows[j].style.color = "white";
+                rows[j].getElementsByTagName('td')[0].style.borderRight = "2px solid #2f3336";
+            }
         }
-        for (let i=0; i<barnames.length; i++){
-            barnames[i].style.background = "#232323";
-            barnames[i].style.color = "white";
+    }
+    let navRows = nav.getElementsByTagName('a');
+    for (let i=0; i<navRows.length; i++){
+        navRows[i].style.color = 'white';
+        navRows[i].style.borderTop = '2px solid #2f3336';
+    }
+    for (let i=0; i<bars.length; i++){
+        bars[i].style.border = "2px solid #2f3336";
+    }
+    for (let i=0; i<barnames.length; i++){
+        barnames[i].style.background = "#232323";
+        barnames[i].style.color = "white";
+    }
+    footer.style.borderTop = "2px solid #2f3336";
+    footer.style.color = "white";
+    moon.style.background = "#f6c246";
+    moon.style.filter = "none";
+    mobMoon.style.background = "#f6c246";
+    mobMoon.style.filter = "none";
+    document.querySelector('#main > div.right_bg').style.borderLeft = '2px solid #2f3336';
+    isDark = true;
+}
+
+function makeLightMob(){
+    bg.style.background = "#f7f7f7";
+    maincont.style.background = "white";
+
+    for (let i=0; i<matchTabs.length; i++){
+        let rows = matchTabs[i].getElementsByTagName('tr');
+        for(let j=0; j<rows.length; j++){
+            if(j===0) {
+                rows[j].style.background = "#33464e";
+                rows[j].style.border = "2px solid #e2e2e2";
+            } else{
+                rows[j].style.border = "2px solid #e2e2e2";
+                rows[j].style.color = "#2d2d2d";
+                rows[j].getElementsByTagName('td')[0].style.borderRight = "2px solid #e2e2e2";
+            }
         }
-        footer.style.borderTop = "1px solid #2f3336";
-        footer.style.color = "white";
-        moon.style.background = "#f6c246";
-        moon.style.filter = "none";
-        mobMoon.style.background = "#f6c246";
-        mobMoon.style.filter = "none";
-        document.querySelector('#main > div.right_bg').style.borderLeft = '1px solid #2f3336';
-        isDark = true;
+    }
+    let navRows = nav.getElementsByTagName('a');
+    for (let i=0; i<navRows.length; i++){
+        navRows[i].style.color = 'black';
+        navRows[i].style.borderTop = '2px solid #e2e2e2';
+    }
+    for (let i=0; i<bars.length; i++){
+        bars[i].style.border = "2px solid #e2e2e2";
+    }
+    for (let i=0; i<barnames.length; i++){
+        barnames[i].style.background = "#33464e";
+        barnames[i].style.color = "#e2e2e2";
+    }
+    footer.style.borderTop = "2px solid #e2e2e2";
+    footer.style.color = "#05172099";
+    moon.style.background = "white";
+    moon.style.filter = "invert(100%)";
+    mobMoon.style.background = "white";
+    mobMoon.style.filter = "invert(100%)";
+    document.querySelector('#main > div.right_bg').style.borderLeft = '2px solid #e7e7e7';
+    isDark = false;
+}
+
+function changeTheme(){
+    const mb = window.matchMedia("(max-width: 1000px)");
+    if(!isDark){
+        if(mb.matches){
+            makeDarkMob();
+        } else{
+            makeDark();
+        }
     }else {
-        bg.style.background = "#f7f7f7";
-        maincont.style.background = "white";
-        const mq = window.matchMedia( "(max-width: 1000px)" );
-        if(!mq.matches){
-            maincont.style.border = "1px solid #e2e2e2";
+        if(mb.matches){
+            makeLightMob();
+        } else{
+            makeLight();
         }
-        for (let i=0; i<matchTabs.length; i++){
-            let rows = matchTabs[i].getElementsByTagName('tr');
-            for(let j=0; j<rows.length; j++){
-                if(j===0) {
-                    rows[j].style.background = "#33464e";
-                    rows[j].style.border = "1px solid #e2e2e2";
-                } else{
-                    rows[j].style.border = "1px solid #e2e2e2";
-                    rows[j].style.color = "#2d2d2d";
-                    rows[j].getElementsByTagName('td')[0].style.borderRight = "1px solid #e2e2e2";
-                }
-            }
-        }
-        let navRows = nav.getElementsByTagName('a');
-        for (let i=0; i<navRows.length; i++){
-            navRows[i].style.color = 'black';
-            navRows[i].style.borderTop = '1px solid #e2e2e2';
-        }
-        for (let i=0; i<bars.length; i++){
-            bars[i].style.border = "1px solid #e2e2e2";
-        }
-        for (let i=0; i<barnames.length; i++){
-            barnames[i].style.background = "#33464e";
-            barnames[i].style.color = "#e2e2e2";
-        }
-        footer.style.borderTop = "1px solid #e2e2e2";
-        footer.style.color = "#05172099";
-        moon.style.background = "white";
-        moon.style.filter = "invert(100%)";
-        mobMoon.style.background = "white";
-        mobMoon.style.filter = "invert(100%)";
-        document.querySelector('#main > div.right_bg').style.borderLeft = '1px solid #e7e7e7';
-        isDark = false;
     }
 }
 
-moon.addEventListener('click', dark);
-mobMoon.addEventListener('click', dark);
+const mob = window.matchMedia("(max-width: 1000px)");
+
+if(isDark){
+    if(mob.matches){
+        makeDarkMob();
+    } else{
+        makeDark();
+    }
+} else{
+    if(mob.matches){
+        makeLightMob();
+    } else{
+        makeLight();
+    }
+}
+
+moon.addEventListener('click', changeTheme);
+mobMoon.addEventListener('click', changeTheme);
